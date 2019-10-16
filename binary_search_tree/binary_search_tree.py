afa719e7f65143beeaa272b43c322fa82ebe17ed
micahjones13 <micahjones13@gmail.com>
2019-10-16 12:18:42 -0600

    binary search tree day 1 done after class

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,7 @@
+from dll_stack import Stack
+from dll_queue import Queue
 import sys
 sys.path.append('../queue_and_stack')
-from dll_queue import Queue
-from dll_stack import Stack
 
 
 class BinarySearchTree:
@@ -10,23 +10,62 @@ class BinarySearchTree:
         self.left = None
         self.right = None
 
-    # Insert the given value into the tree
+    # Insert the given value into the tree | key and value are same in these.
     def insert(self, value):
-        pass
+        if value < self.value:
+            # go left
+            # if there is nothing to the left
+            if not self.left:
+                # insert it. It's a binary search tree.
+                self.left = BinarySearchTree(value)
+            # if there is something there, keep going until there isn't!
+            else:
+                self.left.insert(value)
+        else:
+            # go right
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
+
     def contains(self, target):
-        pass
+        if self.value == target:
+            return True
+
+        if target < self.value:
+            # go left
+            if not self.left:
+                # it's not here
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            # target > value, go right
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        # just keep going right until you can't, then return that value
+        if not self.right:
+            return self.value
+        return self.right.get_max()
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
+    # just modifying values, so no need to return the recursion call
     def for_each(self, cb):
-        pass
+        cb(self.value)
+
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
 
     # DAY 2 Project -----------------------
 
@@ -55,3 +94,22 @@ class BinarySearchTree:
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
         pass
+
+
+"""
+CLASS NOTES
+Left subtree of a node contains only nodes with keys lessar than the node's key.
+THe right subtree of a node contains only nodes with keys greater than the node's key.
+The left and right subtree each must also be a binary search tree.
+
+To search a given key in binary search, we first compare it with root if the key is presten at root, return it. If key is greater
+than the root, we recur for right subtree of root node. otherwise recur for left
+
+If smaller, left. Bigger or Equal, right.
+Doesn't need to be balanced.
+When deleting, smaller child becomes parent
+Deleting root is special. When deleting root, it's replaced by highest node on left side.
+Root starts as first node and stays unless deleted
+Each node is a root for another binary search tree
+
+"""
---
+++ binary_search_tree/dll_stack.py
@@ -0,0 +1,26 @@
+from doubly_linked_list import DoublyLinkedList
+import sys
+sys.path.append('../doubly_linked_list')
+
+
+class Stack:
+    def __init__(self):
+        self.size = 0
+        # Why is our DLL a good choice to store our elements?
+        self.storage = DoublyLinkedList()
+
+    # add to end
+    def push(self, value):
+        self.storage.add_to_tail(value)
+        self.size += 1
+
+    # removes last, returns it
+    def pop(self):
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_from_tail()
+        else:
+            return None
+
+    def len(self):
+        return self.size
